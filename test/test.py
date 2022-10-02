60a647ccfbbed228173accf9d636d34773d4719a
Ed.jones <ejones84@icloud.com>
2022-10-02 16:25:57 +0100

    fix test

--- test/test.py
+++ test/test.py
@@ -7,8 +7,8 @@ import os
 class Test(TestCase):
     def test_london_postcode_to_region(self):
         region = postcode_to_region("W42BE")
-        self.assertEqual("london", region)
+        self.assertEqual(('london', 'london'), region)
 
     def test_somerset_postcode_to_region(self):
         region = postcode_to_region("BS82TU")
-        self.assertEqual("somerset", region)
+        self.assertEqual(('south-west', 'somerset'), region)
